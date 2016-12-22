01c5d65b9326ea3cff6f9b05cd904251a4f3f8a8
wangw571 <wang_w571@hotmail.com>
2016-12-21 23:02:16 -0500

    Yea a lot of things

--- Besiege-Drone-Mod/Besiege-Drone-Mod/Mod.cs
+++ Besiege-Drone-Mod/Besiege-Drone-Mod/Mod.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using spaar.ModLoader;
 using UnityEngine;
-
+using System.Collections.Generic;
+using System.Collections;
 namespace Besiege_Drone_Mod
 {
 
@@ -35,16 +36,20 @@ namespace Besiege_Drone_Mod
 
     public class DroneGeneralConputingScript : MonoBehaviour
     {
-        public int iterativeCount = 0;
-        private GameObject currentTarget;
-        private Vector3 targetPoint;
-        public float 炮弹速度;
-        public Vector3 前一帧速度;
-        public float 目标前帧速度Mag;
-        public float MyPrecision;
-        public int MySize;
+        protected int iterativeCount = 0;
+        protected GameObject currentTarget;
+        protected Vector3 targetPoint;
+        protected float 炮弹速度;
+        protected Vector3 前一帧速度;
+        protected float 目标前帧速度Mag;
+        protected float MyPrecision;
+        protected int MySize;
+        protected GameObject IncomingDetection;
+        protected IncomingDetectionScript IDS;
+        protected bool IgnoreIncoming = false;
 
-        Vector2 formulaProjectile(float X, float Y, float V, float G)
+
+        protected Vector2 formulaProjectile(float X, float Y, float V, float G)
         {
             if (G == 0)
             {
@@ -68,13 +73,13 @@ namespace Besiege_Drone_Mod
             }
         }
 
-        Vector3 formulaTarget(float VT, Vector3 PT, Vector3 DT, float TT)
+        protected Vector3 formulaTarget(float VT, Vector3 PT, Vector3 DT, float TT)
         {
             Vector3 newPosition = PT + DT * (VT * TT);
             return newPosition;
         }
 
-        Vector3 calculateNoneLinearTrajectory(float gunVelocity, float AirDrag, Vector3 gunPosition, float TargetVelocity, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 hitPoint, float G, float accuracy, float diff)
+        protected Vector3 calculateNoneLinearTrajectory(float gunVelocity, float AirDrag, Vector3 gunPosition, float TargetVelocity, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 hitPoint, float G, float accuracy, float diff)
         {
             iterativeCount++;
             if (iterativeCount > 512) { iterativeCount = 0; return hitPoint; }
@@ -119,7 +124,7 @@ namespace Besiege_Drone_Mod
             }
             return calculateNoneLinearTrajectory(gunVelocity, AirDrag, gunPosition, TargetVelocity, TargetPosition, TargetDirection, newHitPoint, G, accuracy, diff1);
         }
-        Vector3 calculateNoneLinearTrajectoryWithAccelerationPrediction(float gunVelocity, float AirDrag, Vector3 gunPosition, float TargetVelocity, float targetAcceleration, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 hitPoint, float G, float accuracy, float diff)
+        protected Vector3 calculateNoneLinearTrajectoryWithAccelerationPrediction(float gunVelocity, float AirDrag, Vector3 gunPosition, float TargetVelocity, float targetAcceleration, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 hitPoint, float G, float accuracy, float diff)
         {
             iterativeCount++;
             if (iterativeCount > 512) { iterativeCount = 0; return TargetPosition; }
@@ -164,7 +169,7 @@ namespace Besiege_Drone_Mod
             }
             return calculateNoneLinearTrajectory(gunVelocity, AirDrag, gunPosition, TargetVelocity, TargetPosition, TargetDirection, newHitPoint, G, accuracy, diff1);
         }
-        Vector3 calculateLinearTrajectory(float gunVelocity, Vector3 gunPosition, float TargetVelocity, Vector3 TargetPosition, Vector3 TargetDirection)
+        protected Vector3 calculateLinearTrajectory(float gunVelocity, Vector3 gunPosition, float TargetVelocity, Vector3 TargetPosition, Vector3 TargetDirection)
         {
 
             Vector3 hitPoint = Vector3.zero;
@@ -198,7 +203,7 @@ namespace Besiege_Drone_Mod
             }
             return hitPoint;
         }
-        Vector3 calculateLinearTrajectoryWithAccelerationPrediction(float gunVelocity, Vector3 gunPosition, float TargetVelocity, float TargetAcceleration, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 PredictedPoint, float Precision)
+        protected Vector3 calculateLinearTrajectoryWithAccelerationPrediction(float gunVelocity, Vector3 gunPosition, float TargetVelocity, float TargetAcceleration, Vector3 TargetPosition, Vector3 TargetDirection, Vector3 PredictedPoint, float Precision)
         {
 
             Vector3 hitPoint = Vector3.zero;
@@ -243,7 +248,7 @@ namespace Besiege_Drone_Mod
             }
         }
 
-        Vector3 getCorrTorque(Vector3 from, Vector3 to, Rigidbody rb, float SpeedPerSecond)
+        protected Vector3 getCorrTorque(Vector3 from, Vector3 to, Rigidbody rb, float SpeedPerSecond)
         {
             try
             {
@@ -256,7 +261,7 @@ namespace Besiege_Drone_Mod
             }
             catch { return Vector3.zero; }
         }
-        AxialDrag AD;
+        protected AxialDrag AD;
 
         void Start()
         {
@@ -265,7 +270,7 @@ namespace Besiege_Drone_Mod
             AD.velocityCap = 300;
             MyPrecision = MySize * 5;
         }
-        Vector3 CalculateTarget(Vector3 LocalTargetDirection, float FireProg)
+        protected Vector3 CalculateTarget(Vector3 LocalTargetDirection, float FireProg)
         {
             炮弹速度 = this.GetComponent<Rigidbody>().velocity.magnitude;
             float targetVelo = currentTarget.GetComponent<Rigidbody>().velocity.magnitude;
@@ -307,5 +312,153 @@ namespace Besiege_Drone_Mod
             return LocalTargetDirection;
         }
 
+    }
+
+    public class IncomingDetectionScript : MonoBehaviour
+    {
+        public Collider Main;
+        public List<Vector3> IncomingPositions;
+        public bool SomethingInMyRange = false;
+
+        void FixedUpdate()
+        {
+            IncomingPositions = new List<Vector3>();
+            SomethingInMyRange = false;
+        }
+
+        void OnTriggerStay(Collider coll)
+        {
+            if (coll == Main)
+            {
+                return;
+            }
+            IncomingPositions.Add(coll.ClosestPointOnBounds(this.transform.position));
+            SomethingInMyRange = true;
+        }
+    }
+
+    public class SmartDrone : DroneGeneralConputingScript
+    {
+        int MissileGuidanceModeInt = 0;
+        int size = 1;
+        float 精度 = 0.02f;
+        int RotatingSpeed = 1;
+        float SphereSize = 15;
+
+        void FixedUpdate()
+        {
+            if (IncomingDetection == null)
+            {
+                IncomingDetection = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                Destroy(IncomingDetection.GetComponent<MeshRenderer>());
+                Destroy(IncomingDetection.GetComponent<Rigidbody>());
+                IDS = IncomingDetection.AddComponent<IncomingDetectionScript>();
+            }
+            Vector3 TargetDirection;
+
+            if (IDS.SomethingInMyRange && !IgnoreIncoming)
+            {
+                Vector3 LocalTargetDirection = this.transform.TransformPoint(RelativeAvenueOfPoints(IDS.IncomingPositions));
+
+                if (MissileGuidanceModeInt == 0)
+                {
+                    LocalTargetDirection = currentTarget.transform.position;
+                    LocalTargetDirection = DroneDirectionIndicator(LocalTargetDirection);
+                }
+
+                //this.transform.rotation.SetFromToRotation(this.transform.forward, LocalTargetDirection);
+                //Vector3 rooo = Vector3.RotateTowards(this.transform.forward, LocalTargetDirection - this.transform.position, RotatingSpeed * size, RotatingSpeed * size);
+                //Debug.Log(LocalTargetDirection + "and" + this.transform.up + "and" + rooo);
+                //this.transform.rotation = Quaternion.LookRotation(rooo);
+                //LocalTargetDirection = new Vector3(LocalTargetDirection.x, LocalTargetDirection.y - this.transform.position.y, LocalTargetDirection.z);
+                //float mag = (LocalTargetDirection.normalized - transform.forward.normalized).magnitude;
+                TargetDirection = (getCorrTorque(this.transform.forward, LocalTargetDirection - this.transform.position * 1, this.GetComponent<Rigidbody>(), 0.01f * size) * Mathf.Rad2Deg).normalized;
+
+                GetComponent<Rigidbody>().angularVelocity = (TargetDirection * RotatingSpeed);
+
+            }
+            else if (currentTarget)
+            {
+                if (currentTarget.GetComponent<Rigidbody>() && currentTarget.transform.position != this.transform.position)
+                {
+                    Vector3 LocalTargetDirection = currentTarget.transform.position;
+
+                    if (MissileGuidanceModeInt == 0)
+                    {
+                        LocalTargetDirection = currentTarget.transform.position;
+                        LocalTargetDirection = DroneDirectionIndicator(LocalTargetDirection);
+                    }
+
+                    //this.transform.rotation.SetFromToRotation(this.transform.forward, LocalTargetDirection);
+                    //Vector3 rooo = Vector3.RotateTowards(this.transform.forward, LocalTargetDirection - this.transform.position, RotatingSpeed * size, RotatingSpeed * size);
+                    //Debug.Log(LocalTargetDirection + "and" + this.transform.up + "and" + rooo);
+                    //this.transform.rotation = Quaternion.LookRotation(rooo);
+                    //LocalTargetDirection = new Vector3(LocalTargetDirection.x, LocalTargetDirection.y - this.transform.position.y, LocalTargetDirection.z);
+                    //float mag = (LocalTargetDirection.normalized - transform.forward.normalized).magnitude;
+                    TargetDirection = (getCorrTorque(this.transform.forward, LocalTargetDirection - this.transform.position * 1, this.GetComponent<Rigidbody>(), 0.01f * size) * Mathf.Rad2Deg).normalized;
+                    if (Vector3.Angle(transform.forward, LocalTargetDirection - this.transform.position * 1) < 105)
+                    {
+                        GetComponent<Rigidbody>().angularVelocity = (TargetDirection * RotatingSpeed);
+                    }
+                    else { Debug.Log("Target Lost!"); }
+                }
+            }
+            前一帧速度 = this.GetComponent<Rigidbody>().velocity;
+        }
+        Vector3 DroneDirectionIndicator(Vector3 LocalTargetDirection)
+        {
+            炮弹速度 = this.GetComponent<Rigidbody>().velocity.magnitude;
+            float targetVelo = currentTarget.GetComponent<Rigidbody>().velocity.magnitude;
+            //Debug.Log((currentTarget.GetComponent<Rigidbody>().velocity - 前一帧速度).magnitude);
+            LocalTargetDirection = calculateNoneLinearTrajectoryWithAccelerationPrediction(
+                炮弹速度 + 0.001f,
+                (this.GetComponent<Rigidbody>().velocity - 前一帧速度).magnitude,
+                transform.position,
+                targetVelo,
+                目标前帧速度Mag - targetVelo,
+                currentTarget.transform.position,
+                currentTarget.GetComponent<Rigidbody>().velocity.normalized,
+                    calculateLinearTrajectoryWithAccelerationPrediction(
+                        炮弹速度,
+                        transform.position,
+                        targetVelo,
+                        targetVelo - 目标前帧速度Mag,
+                        currentTarget.transform.position,
+                        currentTarget.GetComponent<Rigidbody>().velocity.normalized,
+                        calculateLinearTrajectory(
+                            炮弹速度,
+                            transform.position,
+                            targetVelo,
+                            currentTarget.transform.position,
+                            currentTarget.GetComponent<Rigidbody>().velocity.normalized),
+                        size * 精度 + 10 * size
+                    ),
+                    Physics.gravity.y,
+                    size * 精度 + 10 * size,
+                    float.PositiveInfinity
+                    );
+            目标前帧速度Mag = targetVelo;
+
+            if (LocalTargetDirection.y == float.NaN)
+            {
+                LocalTargetDirection = currentTarget.transform.position;
+            }
+            前一帧速度 = GetComponent<Rigidbody>().velocity;
+            return LocalTargetDirection;
+        }
+        Vector3 RelativeAvenueOfPoints(List<Vector3> Vector3s)
+        {
+            Vector3 V3 = this.transform.forward;
+            if (Vector3s.Count > 0)
+            {
+                foreach (Vector3 VT3 in Vector3s)
+                {
+                    Vector3 RealVT3 = (this.transform.position - VT3);
+                    RealVT3 = RealVT3.normalized * (SphereSize - Vector3.Distance(this.transform.position, VT3));
+                    V3 = Vector3.Lerp(V3, RealVT3, 0.5f);
+                }
+            }
+            return V3;
+        }
     }
 }
